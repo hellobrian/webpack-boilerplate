f6854b33c5925d7ef2f5be77ae31e0e7c15c6d40
bthan <bthan@us.ibm.com>
2016-05-22 22:39:04 -0500

    Use webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
+
+// TARGET: variable for npm script in use
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
@@ -16,3 +20,13 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration. We will return this if
+// Webpack is called outside of npm.
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
